6eb0900dbff96d757a1eaa3c5fe578d8208544fc
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-08-04 13:02:56 +0100

    Set environment variables to remove unnecessary Bevy warnings and errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use bevy::{
+    prelude::*,
+    window::{PresentMode, WindowResolution},
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Remove unnecessary vulkan validation errors, PresentMode errors, Xsettings errors, and swapchain errors
+    unsafe {
+        std::env::set_var(
+            "RUST_LOG",
+            "warn,wgpu_hal::vulkan::instance=off,wgpu_hal::vulkan::conv=error,winit::platform_impl::linux::x11::xdisplay=error,bevy_render::view::window=error",
+        );
+    }
+
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Octree Bevy".into(),
+                        resolution: WindowResolution::new(1920., 1080.),
+                        resizable: true,
+                        present_mode: PresentMode::AutoVsync,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+    // circular base
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+    // cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    // camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
